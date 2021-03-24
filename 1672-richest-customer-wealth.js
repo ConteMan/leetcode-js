f5f933e3699dd24a00251deb347399fa16d3c975
ConteMan <boxiaozhi.bolu@gmail.com>
2021-03-24 20:44:22 +0800

    :sparkles:  feat: 1672 最富有客户的资产总和;

---
+++ 1672-richest-customer-wealth.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[][]} accounts
+ * @return {number}
+ */
+ var maximumWealth = function(accounts) {
+  const bankNum = accounts[0].length;
+  const customNum = accounts.length;
+  let maxWealth = 0;
+  for ( let i = 0; i < customNum; i++ ) {
+    let tempMaxWealth = 0;
+    for ( let k = 0; k < bankNum; k++ ) {
+      tempMaxWealth += accounts[i][k];
+    }
+    if (tempMaxWealth > maxWealth) {
+      maxWealth = tempMaxWealth;
+    }
+  }
+  return maxWealth;
+};
